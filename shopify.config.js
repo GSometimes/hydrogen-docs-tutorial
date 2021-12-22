3b8855a663029f3d02532d0547c8b90438886562
Gore Auluck <32429409+GSometimes@users.noreply.github.com>
2021-12-22 12:59:52 -0800

    first commit

---
+++ shopify.config.js
@@ -0,0 +1,6 @@
+export default {
+  locale: 'en-us',
+  storeDomain: 'hydrogen-doc-tutorial.myshopify.com',
+  storefrontToken: '7236d4452d8bd01132153f4a60876e81',
+  graphqlApiVersion: 'unstable',
+};
